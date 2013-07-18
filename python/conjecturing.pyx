d0d667b8af87e411b391cfdc236bc65eaf029ef0
nvcleemp <nico.vancleemput@gmail.com>
2013-07-18 17:08:54 +0200

    Use global variables instead of a tree object.
    This speeds up the generation process, so it's a keeper!

--- python/conjecturing.pyx
+++ python/conjecturing.pyx
@@ -51,107 +51,133 @@ class node:
         else:
             return [self]
 
-class tree:
+#create a tree
+treeRoot = None
+
+cdef int treeDepth = 0
+cdef int treeUCount = 0
+cdef int treeBCount = 0
+
+unusedNodes = []
+nodesAtDepth = {}
+
+cdef int currentUnary = 0
+cdef int currentBinary = 0
     
-    def __init__(self, unary, binary):
-        self.root = node()
-        self.root.depth = 0
-        self.unused = [node() for _ in range(unary + 2*binary)]
+def initTree(int unary, int binary):
+    global treeRoot
+    global treeDepth
+    global treeUCount
+    global treeBCount
+    global unusedNodes
+    global nodesAtDepth
+    global currentUnary
+    global currentBinary
+    
+    treeRoot = node()
+    treeRoot.depth = 0
+    unusedNodes = [node() for _ in range(unary + 2*binary)]
+    
+    nodesAtDepth = {0: [treeRoot]}        
+    
+    treeDepth = 0
+    treeUCount = 0
+    treeBCount = 0
+    
+    currentUnary = unary
+    currentBinary = binary
+    
+def isComplete():
+    return currentUnary == treeUCount and currentBinary == treeBCount
+
+def treeAddChild(parent):
+    global treeDepth
+    global treeUCount
+    global treeBCount
+    
+    child = unusedNodes.pop()
+    parent.addChild(child)
+    child.depth = parent.depth + 1
+    if child.depth > treeDepth: treeDepth = child.depth
+    
+    if parent.type == 2:
+        treeUCount -= 1
+        treeBCount += 1
+    else:
+        treeUCount += 1
         
-        self.nodesAtDepth = {0: [self.root]}        
-        
-        self.depth = 0
-        self.uCount = 0
-        self.bCount = 0
-        
-        self.unary = unary
-        self.binary = binary
+    if child.depth in nodesAtDepth:
+        nodesAtDepth[child.depth].append(child)
+    else:
+        nodesAtDepth[child.depth] = [child]
+
+def treeRemoveChild(parent):
+    global treeDepth
+    global treeUCount
+    global treeBCount
     
-    def isComplete(self):
-        return self.unary == self.uCount and self.binary == self.bCount
+    child = parent.removeChild()
+    unusedNodes.append(child)
     
-    def getOrderedNodes(self):
-        return self.root.getOrderedNodes()
+    nodesAtDepth[child.depth].pop()
+    #child2 = self.nodesAtDepth[child.depth].pop()
+    #assert child == child2, "Should be the same"
+    if not nodesAtDepth[child.depth]: treeDepth -= 1
     
-    def addChild(self, parent):
-        child = self.unused.pop()
-        parent.addChild(child)
-        child.depth = parent.depth + 1
-        if child.depth > self.depth: self.depth = child.depth
-        
-        if parent.type == 2:
-            self.uCount -= 1
-            self.bCount += 1
-        else:
-            self.uCount += 1
-            
-        if child.depth in self.nodesAtDepth:
-            self.nodesAtDepth[child.depth].append(child)
-        else:
-            self.nodesAtDepth[child.depth] = [child]
-    
-    def removeChild(self, parent):
-        child = parent.removeChild()
-        self.unused.append(child)
-        
-        self.nodesAtDepth[child.depth].pop()
-        #child2 = self.nodesAtDepth[child.depth].pop()
-        #assert child == child2, "Should be the same"
-        if not self.nodesAtDepth[child.depth]: self.depth -= 1
-        
-        if parent.type == 1:
-            self.uCount += 1
-            self.bCount -= 1
-        else:
-            self.uCount -= 1
+    if parent.type == 1:
+        treeUCount += 1
+        treeBCount -= 1
+    else:
+        treeUCount -= 1
     
     
-def generateTree(unary, binary):
-    for t in _generateTree(tree(unary, binary)):
-        yield t
+def generateTree(int unary, int binary):
+    initTree(unary, binary)
+    for _ in _generateTree():
+        yield 1
     
-def _generateTree(t):
-    if t.uCount > t.unary + 1 or t.bCount > t.binary:
+def _generateTree():
+    if treeUCount > currentUnary + 1 or treeBCount > currentBinary:
         return
     
-    if t.isComplete():
-        yield t
+    if isComplete():
+        yield 1
         return
     
-    if t.depth == 0:
-        t.addChild(t.root)
-        for nt in _generateTree(t):
-            yield nt
-        t.removeChild(t.root)
+    if treeDepth == 0:
+        treeAddChild(treeRoot)
+        for _ in _generateTree():
+            yield 1
+        treeRemoveChild(treeRoot)
     else:
         #get extensible nodes at level depth - 1
-        start = len(t.nodesAtDepth[t.depth-1]) - 1
-        while start >= 0 and t.nodesAtDepth[t.depth-1][start].type == 0:
+        start = len(nodesAtDepth[treeDepth-1]) - 1
+        while start >= 0 and nodesAtDepth[treeDepth-1][start].type == 0:
             start -= 1
         
-        if start >= 0 and t.nodesAtDepth[t.depth-1][start].type == 1:
+        if start >= 0 and nodesAtDepth[treeDepth-1][start].type == 1:
             start -= 1
             
-        for parent in t.nodesAtDepth[t.depth-1][start+1:]:
-            t.addChild(parent)
-            for nt in _generateTree(t):
-                yield nt
-            t.removeChild(parent)
+        for parent in nodesAtDepth[treeDepth-1][start+1:]:
+            treeAddChild(parent)
+            for _ in _generateTree():
+                yield 1
+            treeRemoveChild(parent)
         
         
         #get extensible (i.e. all) nodes at level depth
-        for parent in t.nodesAtDepth[t.depth]:
-            t.addChild(parent)
-            for nt in _generateTree(t):
-                yield nt
-            t.removeChild(parent)
+        for parent in nodesAtDepth[treeDepth]:
+            treeAddChild(parent)
+            for _ in _generateTree():
+                yield 1
+            treeRemoveChild(parent)
         
 def generateLabeledTree(unary, binary, invariantCount, unaryOperators, binaryNonCommutativeOperators, binaryCommutativeOperators):
     invariants = {'I{}'.format(i):True for i in range(1, invariantCount+1)}
-    for t in generateTree(unary, binary):
-        nodes = t.getOrderedNodes()
+    for _ in generateTree(unary, binary):
+        nodes = treeRoot.getOrderedNodes()
         for _ in _generateLabeledTree(nodes, 0, invariants, unaryOperators, binaryNonCommutativeOperators, binaryCommutativeOperators):
-            yield t
+            yield 1
 
 def _generateLabeledTree(nodes, pos, invariants, unaryOperators, binaryNonCommutativeOperators, binaryCommutativeOperators):
     if pos == len(nodes):
