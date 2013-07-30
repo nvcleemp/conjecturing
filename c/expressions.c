dac1e44d0100af31199219d7590ff2f916f16702
nvcleemp <nico.vancleemput@gmail.com>
2013-07-30 15:01:55 +0200

    Function to trim a string

--- c/expressions.c
+++ c/expressions.c
@@ -536,6 +536,26 @@ void readOperators(FILE *f){
     }
 }
 
+char *trim(char *str){
+    //http://stackoverflow.com/questions/122616/how-do-i-trim-leading-trailing-whitespace-in-a-standard-way
+    char *end;
+    
+    // Trim leading space
+    while(isspace(*str)) str++;
+
+    if(*str == 0)  // All spaces?
+        return str;
+
+    // Trim trailing space
+    end = str + strlen(str) - 1;
+    while(end > str && isspace(*end)) end--;
+
+    // Write new null terminator
+    *(end+1) = 0;
+
+    return str;
+}
+
 //===================================================================
 // Usage methods
 //===================================================================
