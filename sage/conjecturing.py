0c5af8d01597a1d4ce0f3e7c62204c38b7a4ab11
nvcleemp <nico.vancleemput@gmail.com>
2013-08-06 14:11:55 +0200

    Handle functions that do not have the attribute __name__

--- sage/conjecturing.py
+++ sage/conjecturing.py
@@ -3,7 +3,24 @@ sys.path.append(".") # Needed to pass Sage's automated testing
 
 from sage.all import *
 
-def conjecture(objects, invariants, mainInvariant, variableName='x'):
+def conjecture(objects, invariants, mainInvariant):
+    
+    invariantsDict = {}
+    names = []
+    
+    for pos, invariant in enumerate(invariants):
+        if type(invariant) == tuple:
+            name, invariant = invariant
+        elif hasattr(invariant, '__name__'):
+            if invariant.__name__ in invariantsDict:
+                name = '{}_{}'.format(invariant.__name__, pos)
+            else:
+                name = invariant.__name__
+        else:
+            name = 'invariant_{}'.format(pos)
+        invariantsDict[name] = invariant
+        names.append(name)
+
 
     command = './expressions -c --dalmatian --all-operators --time 5 --invariant-names'
 
@@ -15,11 +32,11 @@ def conjecture(objects, invariants, mainInvariant, variableName='x'):
 
     stdin.write('{} {} {}\n'.format(len(objects), len(invariants), mainInvariant))
 
-    for invariant in invariants:
-        stdin.write('{}\n'.format(invariant.__name__))
+    for invariant in names:
+        stdin.write('{}\n'.format(invariant))
     for o in objects:
-        for invariant in invariants:
-            stdin.write('{}\n'.format(invariant(o)))
+        for invariant in names:
+            stdin.write('{}\n'.format(invariantsDict[invariant](o)))
     
     out = sp.stdout
     
