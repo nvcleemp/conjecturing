234d44ce4f0c002f31e60b4b673fd0e42b57bf7e
nvcleemp <nico.vancleemput@gmail.com>
2013-08-12 12:10:16 +0200

    Added option to get all limits at the same time

--- c/expressions.c
+++ c/expressions.c
@@ -1139,7 +1139,10 @@ int processOptions(int argc, char **argv) {
                         inequality = GREATER;
                         break;
                     case 16:
-                        if(strcmp(optarg, "MAX_OBJECT_COUNT")==0){
+                        if(strcmp(optarg, "all")==0){
+                            fprintf(stdout, "MAX_OBJECT_COUNT: %d\n", MAX_OBJECT_COUNT);
+                            fprintf(stdout, "MAX_INVARIANT_COUNT: %d\n", MAX_INVARIANT_COUNT);
+                        } else if(strcmp(optarg, "MAX_OBJECT_COUNT")==0){
                             fprintf(stdout, "%d\n", MAX_OBJECT_COUNT);
                         } else if(strcmp(optarg, "MAX_INVARIANT_COUNT")==0){
                             fprintf(stdout, "%d\n", MAX_INVARIANT_COUNT);
