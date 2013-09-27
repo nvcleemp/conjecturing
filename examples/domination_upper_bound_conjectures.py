119818ea821913f50685034f384b6803bc088401
nvcleemp <nico.vancleemput@gmail.com>
2013-09-27 07:33:47 +0200

    Removed some more known upper bounds

--- examples/domination_upper_bound_conjectures.py
+++ examples/domination_upper_bound_conjectures.py
@@ -22,7 +22,9 @@ def automatedGraphSearch(objects, invariants, minimumVertices, maximumVertices, 
 
 objects = [graphs.CompleteGraph(3)]
 
-invariants.remove(matching_number)
+knownUpperBounds = [matching_number, annihilation_number, fractional_alpha]
+for bound in knownUpperBounds:
+    invariants.remove(bound)
 mainInvariant = invariants.index(dominationNumber) + 1
 
 conjectures = automatedGraphSearch(objects, invariants, 3, 10, mainInvariant=mainInvariant, steps=100)
