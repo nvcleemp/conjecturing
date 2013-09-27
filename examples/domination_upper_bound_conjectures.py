54537d20592734f11388deaf4937c38f954211b3
nvcleemp <nico.vancleemput@gmail.com>
2013-09-27 17:15:32 +0200

    Added possibility to be more verbose

--- examples/domination_upper_bound_conjectures.py
+++ examples/domination_upper_bound_conjectures.py
@@ -3,11 +3,36 @@ This file assumes that the conjecturing spkg and nauty spkg is installed and tha
 'graphtheory.py' are loaded.
 '''
 
-def automatedGraphSearch(objects, invariants, minimumVertices, maximumVertices, upperBound=True, steps=10, mainInvariant=1):
+def automatedGraphSearch(objects, invariants, minimumVertices, maximumVertices, upperBound=True, steps=10, mainInvariant=1, verbose=False):
+    if verbose:
+        print("Starting with these objects:")
+        for g in objects:
+            print("    {}: {}".format(g, g.graph6_string()))
+        print()
+        print("Available invariants:")
+        for pos, invariant in enumerate(invariants):
+            if type(invariant) == tuple:
+                name, _ = invariant
+            elif hasattr(invariant, '__name__'):
+                name = invariant.__name__
+            else:
+                name = 'invariant_{}'.format(pos)
+            if pos + 1 == mainInvariant:
+                print(" *  {}".format(name))
+            else:
+                print("    {}".format(name))
+        print()
     for _ in range(steps):
         l = conjecture(objects, invariants, mainInvariant, upperBound=upperBound)
+        if verbose:
+            print("Found the following conjectures:")
+            for c in l:
+                print("    {}".format(c))
+            print()
         noCounterExample = True
         for i in range(minimumVertices, maximumVertices+1):
+            if verbose:
+                print("Looking for counterexamples with {} {}".format(i, "vertex" if i==1 else "vertices"))
             for g in graphs.nauty_geng('-c {}'.format(i)):
                 if any([not c.evaluate(g) for c in l]):
                     print "Adding {}: {}".format(g, g.graph6_string())
@@ -27,6 +52,6 @@ for bound in knownUpperBounds:
     invariants.remove(bound)
 mainInvariant = invariants.index(dominationNumber) + 1
 
-conjectures = automatedGraphSearch(objects, invariants, 3, 10, mainInvariant=mainInvariant, steps=100)
+conjectures = automatedGraphSearch(objects, invariants, 3, 10, mainInvariant=mainInvariant, steps=100, verbose=True)
 
 print("The conjectures are stored in the variable conjectures.")
