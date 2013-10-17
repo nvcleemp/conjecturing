506fdb1eaaaa75eca6ac9674eeae453ec567139b
nvcleemp <nico.vancleemput@gmail.com>
2013-10-17 08:45:03 +0200

    Examples for Riemann Hypothesis

---
+++ examples/riemann_conjectures1-long.py
@@ -0,0 +1,71 @@
+'''
+This file assumes that the conjecturing spkg is installed and that 'conjecturing.py'
+and 'numbertheory.py' is loaded.
+'''
+
+def automatedSearch(objects, invariants, mainInvariant, universe, upperBound=True, steps=10, verbose=False):
+    if verbose:
+        print("Starting with these objects:")
+        for i in objects:
+            print(" {}".format(i))
+        print("")
+        print("Available invariants:")
+        for pos, invariant in enumerate(invariants):
+            if type(invariant) == tuple:
+                name, _ = invariant
+            elif hasattr(invariant, '__name__'):
+                name = invariant.__name__
+            else:
+                name = 'invariant_{}'.format(pos)
+            if pos + 1 == mainInvariant:
+                print(" * {}".format(name))
+            else:
+                print("   {}".format(name))
+        print("")
+    for step in range(steps):
+        l = conjecture(objects, invariants, mainInvariant, upperBound=upperBound)
+        if verbose:
+            print("Found the following conjectures:")
+            for c in l:
+                print(" {}".format(c))
+            print("")
+        noCounterExample = True
+        for i in universe:
+            try:
+                if any([not c.evaluate(Integer(i)) for c in l]):
+                    print "Step {}: Adding {}".format(step+1, i)
+                    objects.append(i)
+                    universe.remove(i)
+                    noCounterExample = False
+                    break
+            except:
+                print("Error while checking {}".format(i))
+                raise
+        if noCounterExample:
+            print "No counterexample found"
+            break
+    if not noCounterExample:
+        l = conjecture(objects, invariants, mainInvariant, upperBound=upperBound)
+    return l
+
+def riemann(x):
+    return abs(prime_pi(x)-li(x)+li(2))
+
+invariants = [riemann] + invariants
+
+objects = [Integer(3)]
+universe = [Integer(n) for n in range(3, 1000001)]
+
+for n in objects:
+    universe.remove(n)
+
+invariants.remove(('Li', li))
+invariants.remove(('_prime_pi', prime_pi))
+invariants.remove(goldbach) #only for even numbers
+
+mainInvariant = invariants.index(riemann) + 1
+
+conjectures = automatedSearch(objects, invariants, mainInvariant, universe, steps=200, verbose=True, upperBound=True)
+#conjectures = automatedSearch(objects, invariants, mainInvariant, universe, steps=10, verbose=True, upperBound=True)
+
+print("The conjectures are stored in the variable conjectures.")
---
+++ examples/riemann_conjectures1.py
@@ -0,0 +1,22 @@
+'''
+This file assumes that the conjecturing spkg is installed and that 'conjecturing.py'
+and 'numbertheory.py' is loaded.
+'''
+
+def riemann(x):
+    return abs(prime_pi(x)-li(x)+li(2))
+
+invariants = [riemann] + invariants
+
+objects = [3, 10, 16, 28, 27, 29, 36, 37, 11, 13, 17, 18, 30, 31, 5, 35, 4, 12, 32, 34, 38, 39, 40, 41, 46, 42, 49, 50, 51, 52, 53, 54, 57, 58, 59, 60, 62, 63, 64, 65, 66, 70, 72, 78, 71, 61, 79, 80, 81, 88, 89, 91, 93, 94, 95, 96, 97, 98, 99, 100, 101, 103, 121, 122, 123, 124, 125, 110, 114, 115, 117, 126, 127, 128, 129, 130, 131, 113, 134, 135, 136, 137, 139, 144, 145, 146, 147, 148, 149, 151, 161, 102, 138, 162, 166, 174, 175, 176, 177, 178, 179, 180, 182, 187, 189, 9, 190, 191, 169, 192, 193, 195, 197, 198, 200, 208, 210, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 6, 229, 228, 239, 241, 243, 251, 256, 288, 290, 305, 306, 307, 252, 254, 329, 330, 331, 336, 337, 343, 344, 345, 346, 347, 348, 349, 304, 360, 375, 400, 416, 418, 419, 420, 430, 432, 435, 438, 439, 442, 443, 444, 537, 538, 429, 539, 540, 541, 545, 551, 553, 529, 554, 555, 556, 557, 560, 562, 561, 567, 569, 568, 570, 571, 314, 576, 617, 625, 648, 665, 667]
+objects = [Integer(n) for n in objects]
+
+invariants.remove(('li', li))
+invariants.remove(('prime_pi', prime_pi))
+invariants.remove(goldbach) #only for even numbers
+
+mainInvariant = invariants.index(riemann) + 1
+
+conjectures = conjecture(objects, invariants, mainInvariant, upperBound=True)
+
+print("The conjectures are stored in the variable conjectures.")
